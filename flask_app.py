f79f0db0e2b57ae20fe92bffd0b345f0ec68f155
Gamer4Life <uyenwontanswer@gmail.com>
2019-01-07 18:50:03 +0000

    Still working on button functionality

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
