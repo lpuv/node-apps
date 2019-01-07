a071335ef38c61c035a2bedcc5dba921c2af9b64
Gamer4Life <uyenwontanswer@gmail.com>
2019-01-07 22:29:44 +0000

    MySQL part 1

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,29 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
 comments = []
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="pythonUser111",
+    password="@loXlh10",
+    hostname="pythonUser111.mysql.pythonanywhere-services.com",
+    databasename="pythonUser111$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
 
 @app.route("/", methods=["GET", "POST"])
 def index():
